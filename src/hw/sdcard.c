6811f4197d352838642a2ec804fd0e3864434a50
Kamil Wcislo <kamil.wcislo@3mdeb.com>
2017-03-30 16:45:27 +0200

    hw/sdcard: increse sd card frequency and set HIGH_SPEED
    Signed-off-by: Kamil Wcislo <kamil.wcislo@3mdeb.com>

--- src/hw/sdcard.c
+++ src/hw/sdcard.c
@@ -1,6 +1,7 @@
 // PCI SD Host Controller Interface
 //
 // Copyright (C) 2014  Kevin O'Connor <kevin@koconnor.net>
+// Copyright (C) 2016  Eltan B.V.
 //
 // This file may be distributed under the terms of the GNU LGPLv3 license.
 
@@ -94,6 +95,10 @@ struct sdhci_s {
 #define ST_AUTO_CMD12 (1<<2)
 #define ST_READ       (1<<4)
 #define ST_MULTIPLE   (1<<5)
+
+// SDHCI SDHC_CTRL1 Flags
+#define CTRL1_HIGH_SPEED_EN (1<<2)
+#define CTRL1_DAT_TX_WIDTH (1<<1)	// 4 BIT IF SET 0 BIT IF CLEAR (DEFAULT)
 
 // SDHCI capabilities flags
 #define SD_CAPLO_V33             (1<<24)
@@ -458,9 +463,15 @@ sdcard_card_setup(struct sddrive_s *drive, int volt, int prio)
     if (ret)
         return ret;
     // Set controller to data transfer clock rate
-    ret = sdcard_set_frequency(regs, 25000);
+    ret = sdcard_set_frequency(regs, 200000);
     if (ret)
         return ret;
+
+    u32 ctrl1 = readl(&regs->host_control);
+    ctrl1 |= CTRL1_HIGH_SPEED_EN;
+    writel(&regs->host_control, ctrl1);
+    dprintf(3, "host_control contains 0x%08x\n", ctrl1);
+
     // Register drive
     ret = sdcard_get_capacity(drive, csd);
     if (ret)
