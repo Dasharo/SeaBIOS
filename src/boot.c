f28e7cb28beba18a1988b3c966f11a605b0fc938
Michał Żygowski <michal.zygowski@3mdeb.com>
2019-07-05 11:02:32 +0200

    src/boot.c: fail the boot when no HDD found, PXE disabled and menu not entered
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/boot.c
+++ src/boot.c
@@ -518,6 +518,9 @@ get_keystroke(int msec)
  * Boot menu and BCV execution
  ****************************************************************/
 
+static int pxen;
+static int menu_key_pressed = 0;
+
 #define DEFAULT_BOOTMENU_WAIT 2500
 
 // Show IPL option menu.
@@ -527,7 +530,7 @@ interactive_bootmenu(void)
     // XXX - show available drives?
 
     int n_key = 0;
-    int pxen = find_pxen();
+    pxen = find_pxen();
 
     if (! CONFIG_BOOTMENU || !romfile_loadint("etc/show-boot-menu", 1))
         return;
@@ -573,6 +576,7 @@ interactive_bootmenu(void)
     }
     // Show menu items if menu-key is pressed
     else {
+        menu_key_pressed = 1;
         printf("Select boot device:\n\n");
         wait_threads();
 
@@ -634,9 +638,8 @@ static int HaveFDBoot;
 static void
 add_bev(int type, u32 vector)
 {
-    if (type == IPL_TYPE_HARDDISK) {
+    if (type == IPL_TYPE_HARDDISK)
         HaveHDBoot = 1;
-    }
     if (type == IPL_TYPE_FLOPPY && HaveFDBoot++)
         return;
     if (BEVCount >= ARRAY_SIZE(BEV))
@@ -681,10 +684,6 @@ bcv_prepboot(void)
             break;
         }
     }
-
-    // If nothing added a floppy/hd boot - add it manually.
-    add_bev(IPL_TYPE_FLOPPY, 0);
-    add_bev(IPL_TYPE_HARDDISK, 0);
 }
 
 
@@ -821,7 +820,8 @@ do_boot(int seq_nr)
     if (! CONFIG_BOOT)
         panic("Boot support not compiled in.\n");
 
-    if (seq_nr >= BEVCount || BEVCount < 2)
+    if (seq_nr >= BEVCount || (HaveHDBoot == 0 && pxen == 0 &&
+                               menu_key_pressed == 0))
         boot_fail();
 
     // Boot the given BEV type.
@@ -839,8 +839,8 @@ do_boot(int seq_nr)
         boot_cdrom((void*)ie->vector);
         break;
     case IPL_TYPE_CBFS:
-        /* boot from CBFS only when hard disk present and as first selection */
-        if (HaveHDBoot && (seq_nr == 0))
+        /* boot from CBFS only as first selection */
+        if (seq_nr == 0)
             boot_cbfs((void*)ie->vector);
         break;
     case IPL_TYPE_BEV:
